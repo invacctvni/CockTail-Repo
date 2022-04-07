e5922130cc95a601b20cc57b6f87237a0e8428ff
Jiaqi Chen <200477892@student.georgianc.on.ca>
2022-04-07 12:40:03 -0400

    Create drink details fxml, and paste into DrinkDetailsController.

--- src/main/java/com/example/assignment2gc200477892/DrinkDetailsController.java
+++ src/main/java/com/example/assignment2gc200477892/DrinkDetailsController.java
@@ -1,5 +1,31 @@
 package com.example.assignment2gc200477892;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
 public class DrinkDetailsController {
+
+    @FXML
+    private Label IngredientLabel;
+
+    @FXML
+    private Label alcoholLabel;
+
+    @FXML
+    private Label categoryLabel;
+
+    @FXML
+    private Label drinkTitleLabel;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private ListView<?> ingredientListView;
+
+    @FXML
+    private Label instructionLabel;
 
 }
